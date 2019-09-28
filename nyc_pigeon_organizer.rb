734fb562e54bb9ac92bc10f8cd7d036d972a49a4
Ruben Vallejo <r.will16@hotmail.com>
2019-09-28 16:13:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #attributes are :color,:gender,:lives
+  #inner_keys are color range,gender and location keys
+  
+  new_hash = {}
+  inner_hash = {}
+  
+  data.each do |attributes, name_hash|
+    name_hash.each do |inner_keys, name_array|
+      name_array.each do |pigeon_name|
+        inner_hash[attributes] = []
+        
+      if !new_hash[pigeon_name]
+          new_hash[pigeon_name] = inner_hash
+      end
+      
+      if name_hash[inner_keys].include? pigeon_name #&& new_hash[pigeon_name][attributes] == attributes
+          inner_hash[attributes].push(inner_keys.to_s)
+      end
+      
+      
+      end
+    end
+  end
+ # inner_hash
+  new_hash
+  p data
+  p new_hash
 end
