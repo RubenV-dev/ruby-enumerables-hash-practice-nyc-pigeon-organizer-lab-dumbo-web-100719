76b1e903ab56088e2bdeed5c850177266f52f5f8
Ruben Vallejo <r.will16@hotmail.com>
2019-09-28 16:26:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,9 +8,9 @@ def nyc_pigeon_organizer(data)
   data.each do |attributes, name_hash|
     name_hash.each do |inner_keys, name_array|
       name_array.each do |pigeon_name|
-        
+        inner_hash[attributes] = []
       if !new_hash[pigeon_name]
-          inner_hash[attributes] = []
+          #inner_hash[attributes] = []
           new_hash[pigeon_name] = inner_hash
       end
       #if name_hash[inner_keys].include? pigeon_name #&& new_hash[pigeon_name][attributes] == attributes
